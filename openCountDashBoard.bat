7e6f57cd0890abad70d4762f012e5e6e0dc959f7
helloworld2523 <helloworld2523@gmail.com>
2025-02-03 14:36:44 +0700

    ปรับปรุง.batง

--- openCountDashBoard.bat
+++ openCountDashBoard.bat
@@ -1,10 +1,13 @@
 @echo off
 cd /d C:\Users\RU\Documents\yolov5peoplecounterwin11-main\app 
 call .\venv\Scripts\activate
-python .\app.py
+start python .\app.py
+
+:: รอ 5 วินาที (หรือปรับค่าเป็นเวลาที่ต้องการ)
+timeout /t 5 /nobreak
 
 
-:: เปิด Google Chrome ไปที่ URL ที่ต้องการ
-start chrome http://127.0.0.1:5000/
-start chrome http://127.0.0.1:5000/monitor
-start chrome http://127.0.0.1:5000/edit_queue
+:: เปิด Chrome ผ่านพาธที่แน่นอน
+start ""  http://127.0.0.1:5000/
+start ""  http://127.0.0.1:5000/monitor
+start ""  http://127.0.0.1:5000/edit_queue
